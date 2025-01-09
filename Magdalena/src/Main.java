6f04db771ea2af59cb12c4e687bfb4218b560af3
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-01-09 10:02:20 +0100

    Add files via upload

---
+++ Magdalena/src/Main.java
@@ -0,0 +1,19 @@
+import pojo.Magdalena;
+
+public class Main {
+    public static void main(String[] args) {
+        //MAGDALENA
+        Magdalena magdalena = new Magdalena("Chocolate", "Marrón", "Harina", "Vainilla");
+            magdalena.hornear();
+            magdalena.mezclar();
+        /*  magdalena.vender();
+            magdalena.decorar();
+            magdalena.establecerSabor();
+            magdalena.amasar();
+            magdalena.mojar();
+            magdalena.anyadirPepitas();
+            magdalena.comer();
+            magdalena.envolver();
+            magdalena.coger(); */
+    }
+}
