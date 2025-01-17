551d43ad3678278577fcda2bc0c2721fcc621dd1
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-01-17 11:14:33 +0100

    Add files via upload

--- 02 - Netflix/index.js
+++ 02 - Netflix/index.js
@@ -1,46 +1,55 @@
 // API REST
 
-//IMPORTS EN JAVA
-const express = require("express");  //API REST -> NODE JS CON EXPRESS
-const { Pool } = require("pg"); //HABLAR BD PG DE AWS
+// IMPORTS EN JAVA
+const express = require("express"); // API REST -> NODE JS CON EXPRESS
+const { Pool } = require("pg");      // HABLAR BD PG DE AWS
 
 // INSTANCIAR LOS OBJETOS QUE NECESITAMOS
-const app  = express();
+const app = express();
 const port = 3000;
- //CONSULTAR SELECT * FROM USUARIOS, PELICULAS
-app.get("/usuarios/" , (req , res) => {
-    // req -> no lo necesito
-    // res -> si
-    res.send('Has solicitado una lista de usuarios');
-
-});
-
+// Configuración de la base de datos
 const pool = new Pool({
     user: "postgres",
-    host: "netflix-01.c3smyaeku6nl.us-east-1.rds.amazonaws.com",
+    host: "netflix-001.c3smyaeku6nl.us-east-1.rds.amazonaws.com",
     database: "postgres",
-    password: "rerreF_2013",
-    port: 5432
-})
-
-app.get("/peliculas", async (req, res) =>{
-    const res = pool.query("SELECT * FROM PELICULAS")
-    res.json(result.rows);
+    password: "LUCASLUCAS", // Considera usar variables de entorno para gestionar contraseñas
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false, // Cambia a false si tienes problemas de certificados pero trata de evitarlo por seguridad
+      // ca: fs.readFileSync('/path/to/server-ca.pem').toString(),
+      // Es posible que AWS RDS requiera parámetros SSL específicos o archivos CA.
+      // Comprueba la documentación de AWS RDS para obtener los detalles exactos.
+    },
+  });
+  app.get("/peliculass", async (req, res)=>{
+    const {rows} = await pool.query(
+        "SELECT * FROM peliculas;"
+    );
+    res.json(rows);
+    // res.send("Bienvenido a mi API DISNEY");
 });
+    
 
-app.get("/usuarios/:id", (req, res) =>{
-    const userId = req.params.id;
-    res.send(`El id del usario es: ${userId}`);
-});
+    // CONSULTAR -> SELECT * FROM USUARIOS, PELICULAS
+    app.get("/usuarios/", (req, res) =>{
+        // req -> no lo necesito
+        // res -> sí
+        res.send('Has solicitado una lista de usuarios');
+    }); 
 
-// ---
-app.listen(port, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+    app.get("/usuarios/:id", (req, res) =>{
+        const userId = req.params.id;
+        res.send(`El ID del usuario es: ${userId}`);
+    });
 
-//LOGIN, PELICULAS POR CATEGORIAS
- // ADD -> INSERT
-// app.post("/usuarios/" (req , res)); 
-// ELIMINAR -> DELETE             
-// app.delete("/usuarios/"); 
-// app.put("/usuarios/"); //MODIFICAR -> UPDATE
+    // ----
+    app.listen(port, () => {
+        console.log(`Servidor corriendo en http://localhost:${port}`);
+      });
+    // LOGIN, PELÍCULAS POR CATEGORÍAS
+        // ADD -> INSERT
+        //     app.post("/usuarios/", (req, res)); 
+    // ELIMINAR -> DELETE                            
+        // app.delete("/usuarios/", (req, res)); 
+    // MODIFICAR -> UPDATE
+        // app.put("/usuarios/", (req, res));    
