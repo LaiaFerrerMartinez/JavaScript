c61b91293be330345ac0c81a14e53141a59d0ff8
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-01-30 13:52:08 +0100

    Add files via upload

--- 07 - Front Api Rest/main.js
+++ 07 - Front Api Rest/main.js
@@ -1,3 +1,10 @@
+const API_URL = "http://127.0.0.1:3000";
+const GET_PELICULAS = API_URL + "/peliculas";
+const GET_PELICULAS_BYGENERO = API_URL + "";
+const GET_PELICULAS_FAVORITAS = API_URL + "";
+const INSERT_PELICULAS = API_URL + "";
+const GET_USUARIOS = API_URL + "/usuarios/:id";
+
 function getPeliculas() {
     alert("PASO 1");
 }
