7616dbf46cd55a9d614f86001a45287e50eeff64
Laia Ferrer Martínez <laia.ferrer.martinez@gmail.com>
2025-02-06 11:32:31 +0100

    Add files via upload

--- 09 - Log In/index.js
+++ 09 - Log In/index.js
@@ -5,13 +5,42 @@
         // request, response
         const express = require('express');
         const {Pool} = require('pg');
+        const cors = require("cors");
 
         // INSTANCIAR
         const app = express();
         const pool = new Pool();
+        const port = 3000;
 
         // CORS
         app.use(cors());
 
         //JSON
         app.use(express.json());
+
+        // GETTER
+            // SELECT
+            app.get('/api/peliculas?USER=A&PASSWORD=1234', async(req, res) => {
+
+            });
+        // PUT
+            // UPDATE
+        // POST
+            // INSERT
+                // LOGIN
+                app.post('/api/login', async(req, res) => {
+                    // RECUPERAR PARÁMETROS SI CORRESPONDE
+                        // BODY.PARAMS
+                        // QUERY.PARAMS
+                        // PATH.PARAMS
+                        const {username, password} = req.body;
+                });
+        // DELETE
+            // DELETE
+
+
+
+
+            app.listen(port, () => {
+                console.log(`Servidor corriendo en http://localhost:${port}`);
+              });
